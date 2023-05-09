561f038a0901966375afaf411e70c2ac40b5c87d
anaezquerro <anaezquerro0701@gmail.com>
2023-05-09 20:53:42 +0200

    annoy indexer

--- main.py
+++ main.py
@@ -126,7 +126,7 @@ def track2vec(args):
     if 'recommend' in args.action:
         start = time.time()
         model.recommend(submit_path=args.submit_path, num_threads=args.num_threads[i],
-                        batch_size=args.batch_size, verbose=args.verbose)
+                        batch_size=args.batch_size, num_trees=args.num_trees, annoy=args.annoy, verbose=args.verbose)
         end = time.time()
         if args.time:
             print('Recommending time:', end-start)
@@ -191,7 +191,8 @@ if __name__ == '__main__':
     track2vec_parser.add_argument('--model_path', type=str, default='data/track2vec', help='Path to store Gensim model')
     track2vec_parser.add_argument('--S_path', type=str, default='data/S-track2vec.npz')
     track2vec_parser.add_argument('--num_epochs', type=int, default=50, help='Number of epochs in training')
-    track2vec_parser.add_argument('--granularity', type=int, default=int(10e3), help='Number of trees for the Annoy Index')
+    track2vec_parser.add_argument('--num_trees', type=int, default=10, help='Number of trees for the Annoy Index')
+    track2vec_parser.add_argument('--annoy', action='store_true', help='Approximate nearest neighbours by cosine similarity')
     track2vec_parser.add_argument('--batch_size', type=int, default=100)
     add_paths(track2vec_parser)
 
--- models/track2vec.py
+++ models/track2vec.py
@@ -1,5 +1,5 @@
 from gensim.models import Word2Vec
-from scipy.sparse import csr_matrix, load_npz, save_npz
+from scipy.sparse import csr_matrix, load_npz, save_npz, vstack
 from utils.tools import tolist, load_pickle, read_json, pop_empty, coalesce
 from typing import List, Tuple, Dict
 from utils.constants import TEST_FILE, N_RECS, MAX_THREADS, INFO_ROW
@@ -9,7 +9,8 @@ from concurrent.futures import ProcessPoolExecutor
 from gensim.similarities.annoy import AnnoyIndexer
 from models.neighbour import recommend, csr_argsort
 class Track2VecModel:
-    def __init__(self, embed_dim: int, context_size: int, k: int, model_path: str, train_path: str, test_path: str, trackmap_path: str, S_path: str):
+    def __init__(self, embed_dim: int, context_size: int, k: int, model_path: str, train_path: str, test_path: str,
+                 trackmap_path: str, S_path: str):
         self.embed_dim = embed_dim
         self.context_size = context_size
         self.model_path = model_path
@@ -18,6 +19,7 @@ class Track2VecModel:
         self.S_path = S_path
         self.trackmap_path = trackmap_path
         self.k = k
+
 
     def load(self):
         self.model = Word2Vec.load(self.model_path)
@@ -35,10 +37,10 @@ class Track2VecModel:
         self.model.save(self.model_path)
 
 
-    def item_similarity(self, batch_size: int, num_threads: int, verbose: int) -> csr_matrix:
+    def pure_similarity(self, batch_size: int, num_threads: int, verbose: int) -> csr_matrix:
 
         if verbose:
-            print('Computing item similarity for all tracks')
+            print('Computing pure item similarity for all tracks')
 
         embeds = np.array([self.model.wv[track] for track in range(len(self.model.wv))])
         self.model = None
@@ -49,12 +51,12 @@ class Track2VecModel:
 
             for i in range(0, embeds.shape[0], batch_size):
                 futures.append(
-                    pool.submit(similarity, i, batch_size, embeds, self.k, track_norm, verbose)
+                    pool.submit(pure_similarity, i, batch_size, embeds, self.k, track_norm, verbose)
                 )
 
             S = csr_matrix((embeds.shape[0], embeds.shape[0]))
             for _ in range(len(futures)):
-                S_partial = similarity(i, batch_size, embeds, self.k, track_norm, verbose)
+                S_partial = futures.pop(0)
                 S[i:(i + S_partial.shape[0]), :] = S_partial
                 S[:, i:(i + S_partial.shape[0])] = S_partial.transpose()
 
@@ -63,12 +65,37 @@ class Track2VecModel:
                 cols, values = map(lambda x: x.flatten().tolist(), (cols, values))
                 rows = np.arange(S_partial.shape[0])
                 S = csr_matrix((values, (rows, cols)), shape=(embeds.shape[0], embeds.shape[0]))
+
         save_npz(file=self.S_path, matrix=S)
         return S
 
-    def recommend(self, submit_path: str, num_threads: int, batch_size: int, verbose: bool):
+    def annoy_similarity(self, num_threads, num_trees: int, verbose: int) -> csr_matrix:
+        if verbose:
+            print('Computing item similarity for all tracks with Annoy Indexer')
+
+        with ProcessPoolExecutor(max_workers=num_threads) as pool:
+            futures = list()
+            indexes = coalesce(len(self.model.wv), num_threads)
+            for i in range(num_threads):
+                start, end = indexes[i], indexes[i+1]
+                futures.append(
+                    pool.submit(annoy_similarity, start, end, self.model, self.k, num_trees, verbose)
+                )
+
+            S = futures.pop(0).result()
+            for _ in range(len(futures)):
+                S = vstack([S, futures.pop(0).result()])
+
+        save_npz(file=self.S_path, matrix=S)
+        return S
+
+
+    def recommend(self, submit_path: str, num_threads: int, batch_size: int, annoy: bool, num_trees: int, verbose: bool):
         # first we must compute the similarity matrix
-        S = self.item_similarity(batch_size, num_threads, verbose)
+        if annoy:
+            S = self.annoy_similarity(num_threads, num_trees, verbose)
+        else:
+            S = self.pure_similarity(batch_size, num_threads, verbose)
 
         # now obtain the estimated ratings
         Rtest = load_npz(self.test_path)
@@ -94,7 +121,7 @@ class Track2VecModel:
             indexes = coalesce(len(pids), num_threads)
 
             for i in range(num_threads):
-                start, end = indexes[i], indexes[i + 1]
+                start, end = indexes[i], min(indexes[i + 1], len(self.model.wv))
                 futures.append(
                     pool.submit(
                         recommend,
@@ -117,7 +144,7 @@ class Track2VecModel:
                 file.write(f'{pid},' + ','.join(list(map(trackmap.get, popular))) + '\n')
 
 
-def similarity(i: int, batch_size: int, embeds: np.ndarray, k: int, track_norm: np.ndarray, verbose: bool):
+def pure_similarity(i: int, batch_size: int, embeds: np.ndarray, k: int, track_norm: np.ndarray, verbose: bool):
     if verbose:
         print(f'Computing similarity for track {i}/{embeds.shape[0]}')
     v = embeds[i:(i+batch_size)]
@@ -133,17 +160,29 @@ def similarity(i: int, batch_size: int, embeds: np.ndarray, k: int, track_norm: 
     return S
 
 
+def annoy_similarity(start: int, end: int, model: Word2Vec, k: int, num_trees: int, verbose: bool):
+    info = lambda i: print(f'Computing similarity for track {i}/{len(model.wv)}') if verbose and (i%1000 == 0) else None
+    indexer = AnnoyIndexer(model, num_trees=num_trees)
+    rows, cols, values = list(), list(), list()
+    tracks = list(range(start, end))
+    for i, track in enumerate(tracks):
+        info(track)
+        neighbours, sims = map(list, zip(*model.wv.most_similar(model.wv[i], topn=k+1, indexer=indexer)))
+        if track in neighbours:
+            index = neighbours.index(track)
+            neighbours.pop(index)
+            sims.pop(index)
+        else:
+            neighbours.pop(-1)
+            sims.pop(-1)
+        rows += ([i]*k)
+        cols += neighbours
+        values += sims
+    S = csr_matrix((values, (rows, cols)), shape=(len(tracks), len(model.wv)))
+    return S
 
 
 
 
-if __name__ == '__main__':
-    model = Track2VecModel(embed_dim=100, context_size=20, model_path='data/track2vec',
-                           train_path='data/Rtrain.npz', test_path='data/Rtest.npz', trackmap_path='data/trackmap.pickle',
-                           k=10)
-    model.load()
-    start = time.time()
-    model.recommend('submissions/embed.csv.gz', batch_size=500, num_threads=10, verbose=True)
-    end = time.time()
 
 
